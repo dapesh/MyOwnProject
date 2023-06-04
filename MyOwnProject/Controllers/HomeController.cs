ee976eecdd427bdc5cbab6b1ac5556a212652b77
Dipesh Wagle <dipesh.wagle@inficare.com.my>
2023-06-04 22:04:55 +0545

    Uploaded images locally

--- MyOwnProject/Controllers/HomeController.cs
+++ MyOwnProject/Controllers/HomeController.cs
@@ -2,7 +2,8 @@
 using MyOwnProject.Data;
 using MyOwnProject.Models;
 using MyOwnProject.Services;
-using System.Diagnostics;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Hosting;
 
 namespace MyOwnProject.Controllers
 {
@@ -10,14 +11,18 @@ namespace MyOwnProject.Controllers
     {
         private readonly ICompanyRepository _companyRepository;
         private readonly ApplicationDbContext _db;
-        public HomeController(ICompanyRepository companyRepository, ApplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(ICompanyRepository companyRepository, ApplicationDbContext db,IWebHostEnvironment webHostEnvironment)
         {
             _companyRepository = companyRepository;
             _db=db;
+            _webHostEnvironment=webHostEnvironment;
         }
         public async Task<ActionResult> Index()
         {
             var company = await _companyRepository.GetCompanies();
+
             //Console.WriteLine(company);
             return View(company);
         }
@@ -60,8 +65,19 @@ namespace MyOwnProject.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult InsertCompany(Company insertCompany)
+        public ActionResult InsertCompany(Company insertCompany, IFormFile ImageFile)
         {
+            if(ImageFile != null && ImageFile.Length>0)
+            {
+                string fileName = Path.GetFileName(ImageFile.FileName);
+                string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Images", fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    ImageFile.CopyTo(fileStream);
+                }
+                insertCompany.CompanyLogo = "/Images/" + fileName;
+            }
+            //return RedirectToAction("Index");   
             if (ModelState.IsValid)
             {
                 _db.Companies.Add(insertCompany);
